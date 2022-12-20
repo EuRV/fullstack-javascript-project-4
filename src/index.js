b97136da789fd45ce2fc53444bdcb94cbec81a18
EuRV <eugenirovensky@yandex.ru>
2022-12-20 17:44:08 +0600

    refactoring: change the structure of the module index.js
    Transfer the download from the links to a separate function loadAndSaveFiles.

--- src/index.js
+++ src/index.js
@@ -4,37 +4,35 @@ import path from 'path';
 import { cwd } from 'process';
 import { convertUrlToPath, getPageContentAndDownloadLinks } from './utilities.js';
 
-const pageLoader = (link, outputPath = cwd()) => {
-  const nameAssetFolder = convertUrlToPath(link, '_files');
-  const pathToDirAssets = path.join(outputPath, nameAssetFolder);
+const loadAndSaveFiles = (promises, pathToCurrentDir) => (
+  Promise.all(promises.map(({ linkToAsset, pathToAsset }) => (
+    axios.get(linkToAsset, { responseType: 'arraybuffer' })
+      .then((response) => fsp.writeFile(path.join(pathToCurrentDir, pathToAsset), response.data))
+  )))
+);
 
+const pageLoader = (link, outputPath = cwd()) => {
+  const nameAssetsFolder = convertUrlToPath(link, '_files');
+  const pathToDirAssets = path.join(outputPath, nameAssetsFolder);
   const fileName = convertUrlToPath(link, '.html');
   const pathToHtmlFile = path.join(outputPath, fileName);
 
-  let downloadLinks;
-  let pageContent;
   return axios.get(link, { responseType: 'arraybuffer' })
     .then(({ data }) => {
-      ({ pageContent, downloadLinks } = getPageContentAndDownloadLinks(
+      const { pageContent, downloadLinks } = getPageContentAndDownloadLinks(
         data,
         link,
-        nameAssetFolder,
-      ));
+        nameAssetsFolder,
+      );
       fsp.writeFile(pathToHtmlFile, pageContent);
+      return downloadLinks;
     })
-    .then(() => {
+    .then((downloadLinks) => {
       if (downloadLinks.length === 0) {
         return null;
       }
       fsp.mkdir(pathToDirAssets, { recursive: true });
-      return Promise.all(downloadLinks.map((downloadLink) => {
-        const contentName = convertUrlToPath(downloadLink);
-        return axios.get(downloadLink, { responseType: 'arraybuffer' })
-          .then((response) => fsp.writeFile(path.join(
-            pathToDirAssets,
-            contentName,
-          ), response.data));
-      }));
+      return loadAndSaveFiles(downloadLinks, outputPath);
     })
     .then(() => pathToHtmlFile);
 };
