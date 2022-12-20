c6bd076ab231a765d95b9c0460c7d5692ac2dc6c
EuRV <eugenirovensky@yandex.ru>
2022-12-20 22:25:55 +0600

    refactor: rename vars, convert a link to a url object

--- src/index.js
+++ src/index.js
@@ -4,24 +4,25 @@ import path from 'path';
 import { cwd } from 'process';
 import { convertUrlToPath, getPageContentAndDownloadLinks } from './utilities.js';
 
-const loadAndSaveFiles = (promises, pathToCurrentDir) => (
-  Promise.all(promises.map(({ linkToAsset, pathToAsset }) => (
+const loadAndSaveFiles = (arrayPathsAndLinks, pathToCurrentDir) => (
+  Promise.all(arrayPathsAndLinks.map(({ linkToAsset, pathToAsset }) => (
     axios.get(linkToAsset, { responseType: 'arraybuffer' })
       .then((response) => fsp.writeFile(path.join(pathToCurrentDir, pathToAsset), response.data))
   )))
 );
 
 const pageLoader = (link, outputPath = cwd()) => {
-  const nameAssetsFolder = convertUrlToPath(link, '_files');
+  const url = new URL(link);
+  const nameAssetsFolder = convertUrlToPath(url, '_files');
   const pathToDirAssets = path.join(outputPath, nameAssetsFolder);
-  const fileName = convertUrlToPath(link, '.html');
+  const fileName = convertUrlToPath(url, '.html');
   const pathToHtmlFile = path.join(outputPath, fileName);
 
   return axios.get(link, { responseType: 'arraybuffer' })
     .then(({ data }) => {
       const { pageContent, downloadLinks } = getPageContentAndDownloadLinks(
         data,
-        link,
+        url,
         nameAssetsFolder,
       );
       fsp.writeFile(pathToHtmlFile, pageContent);
