749ac625baac4414084dff911a16767979e08b2d
EuRV <eugenirovensky@yandex.ru>
2022-12-20 22:13:53 +0600

    test: change the testing parameters of the getPageContentAndDownloadLinks function

--- __tests__/utilities.test.js
+++ __tests__/utilities.test.js
@@ -7,8 +7,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const link = 'https://ru.hexlet.io/courses';
-const pathToDir = '/home/eurv/learning_project/hexlet/fullstack-javascript-project-4/ru-hexlet-io-courses_files';
+const link = new URL('https://ru.hexlet.io/courses');
+const pathToDir = 'ru-hexlet-io-courses_files';
 
 test('getPageContentAndDownloadLinks', async () => {
   const originalHtml = await fsp.readFile(getFixturePath('page-with-links.html'), 'utf-8');
