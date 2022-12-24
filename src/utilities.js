d81727f0f3494faf9d09d0f43fad223e7a94bb0f
EuRV <eugenirovensky@yandex.ru>
2022-12-24 21:50:44 +0600

    refactor: fix getPageContentAndDownloadLinks function

--- src/utilities.js
+++ src/utilities.js
@@ -2,8 +2,6 @@ import path from 'path';
 import * as cheerio from 'cheerio';
 import prettier from 'prettier';
 
-const isCorrectHostname = (link1, link2) => link1.hostname === link2.hostname;
-
 const convertUrlToPath = (url, ending = '') => {
   const { hostname, pathname } = url;
   let basename = '';
@@ -19,18 +17,16 @@ const getPageContentAndDownloadLinks = (data, url, pathToDir) => {
   const tags = { link: 'href', img: 'src', script: 'src' };
   const $ = cheerio.load(data);
   const downloadLinks = Object.entries(tags).reduce((acc, [tag, atr]) => {
-    const pathToContent = $(tag).map((i, el) => {
-      let downloadPaths;
-      const pathToDownloadedAsset = $(el).attr(atr);
-      const linkToAsset = new URL(pathToDownloadedAsset, url);
-      if (isCorrectHostname(url, linkToAsset)) {
+    const pathToContent = $(tag).map((i, el) => $(el)).toArray()
+      .filter(($element) => $element.attr(atr) !== undefined)
+      .map(($element) => ({ $element, linkToAsset: new URL($element.attr(atr), url) }))
+      .filter(({ linkToAsset }) => linkToAsset.hostname === url.hostname)
+      .map(({ $element, linkToAsset }) => {
         const nameAsset = !path.extname(linkToAsset.pathname) ? convertUrlToPath(linkToAsset, '.html') : convertUrlToPath(linkToAsset);
         const pathToAsset = path.join(pathToDir, nameAsset);
-        $(el).attr(atr, pathToAsset);
-        downloadPaths = { linkToAsset, pathToAsset };
-      }
-      return downloadPaths;
-    }).toArray();
+        $element.attr(atr, pathToAsset);
+        return { linkToAsset, pathToAsset };
+      });
     return [...acc, ...pathToContent];
   }, []);
   const pageContent = prettier.format($.html(), { parser: 'html' });
